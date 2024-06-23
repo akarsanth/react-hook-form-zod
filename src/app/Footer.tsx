3ad529342c6d9a14045465a3b032590dc9bfe635
Aakarshan Thapa <akarsan.thapa@gmail.com>
2024-06-23 17:00:57 -0400

    Complete footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 // Learn testing as well
 export const Footer = () => {
   return (
-    <div className="h-20 border-t mt-16">
-      <div className="container max-w-screen-lg mx-auto">Footer</div>
-    </div>
+    <footer className="h-20 border-t mt-16">
+      <div className="container max-w-screen-lg h-full mx-auto flex flex-col items-center justify-center gap-0.5">
+        <a
+          href="https://github.com/akarsanth/react-hook-form-zod"
+          target="_blank"
+          className="p-1.5 hover:bg-pink-200 dark:hover:bg-pink-600  rounded-full"
+        >
+          <FaGithub className="text-xl" />
+        </a>
+
+        <p className="text-xs font-light tracking-wide">
+          Made by Aakarshan for Learning.
+        </p>
+      </div>
+    </footer>
   );
 };
